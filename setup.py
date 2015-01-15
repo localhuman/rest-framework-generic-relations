5d07b88dadbd8d9109e9801d5ecc47ae01315ab7
Thomas Saunders <tasaunders@gmail.com>
2015-01-15 16:22:47 -0600

    more setup edits

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.3.0'
+version = '0.3.1'
 
 install_requires = [
     'djangorestframework>=3.0.0',
@@ -12,12 +12,12 @@ install_requires = [
 
 
 setup(
-    name='rest-framework-generic-relations',
+    name='rest-framework-generic-relations-localhuman-fork',
     version=version,
     url='https://github.com/localhuman/rest-framework-generic-relations',
     license='BSD',
-    description='Generic Relations for Django Rest Framework',
-    author='Ian Foote',
+    description='Generic Relations for Django Rest Framework - localhuman-fork',
+    author='localhuman',
     author_email='python@ian.feete.org',
     packages=find_packages(),
     include_package_data=True,
